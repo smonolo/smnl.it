4a15e95081eb3c34c2cb581fda6a80ae02a8f7e2
s.monolo <lu3mtex@gmail.com>
2023-04-23 02:57:16 +0200

    add page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Fragment } from 'react';
+import Head from 'next/head';
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Component {...pageProps} />
+        <Fragment>
+            <Head>
+                <title>Stefano</title>
+            </Head>
+            <Component {...pageProps} />
+        </Fragment>
     );
 }
